bfcb8340aea92174256cfb33956c2c7c8e0c8fa9
=mustafa0624 <mustafa.yucel0624@gmail.com>
2020-09-28 19:47:01 +0200

    feat:add numbers and operators

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-document.getElementsByClassName("button").addEventListener("click",screen());
-var deger = document.getElementsByClassName("button").value;
+let topScreen = document.querySelector(".top_screen");
+let bottomScreen = document.querySelector(".bottom_screen")
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let ac = document.querySelector(".ac");
+let del = document.querySelector(".del");
+let equal = document.querySelector(".equal");
+let operatorValue = ""
 
-function screen(){
+numbers.forEach(function(touch){
+    touch.addEventListener("click", () => {
+        bottomScreen.innerText += touch.innerText})
+        
 
-    document.getElementById("form-input").innerHTML=deger;
+})
+
+operators.forEach(function(opr){
+    opr.addEventListener("click",() => {
+        operatorValue = opr.innerText;
+        
+        computation(operatorValue);
+        display();
+        
+       
+        })
+})
+
+
+function computation(operatorValue){
+    if(topScreen.innerHTML == ""){return}
+   switch(operatorValue){
+       case "+":
+        topScreen.innerText += Number(bottomScreen.innerText) + Number(topScreen.innerText.slice(0,-2));
+   }
+
+   
+
 }
 
 
+
+function display(){
+    topScreen.innerText = bottomScreen.innerText + " " + operatorValue;
+        bottomScreen.innerText = "";
+
+}
 
